6537f45df692838b913aafc5906854f46b5845e2
dsuth10 <54962809+dsuth10@users.noreply.github.com>
2026-03-04 09:34:59 +1000

    feat: add event data and detail pages for federation figures

---
+++ Units/HASS/Unit 1/australian-history-hub v2/edmund-barton/event-data.js
@@ -0,0 +1,50 @@
+const eventData = {
+    "1849": {
+        title: "A Boy Named Toby",
+        image: "../assets/img/edmund_barton_portrait_vintage.png",
+        content: `
+            <p>Edmund Barton was born in Sydney. His father was an accountant, and his mother ran a school for girls. He was the youngest of nine children!</p>
+            <p>He was a very smart boy and was always the top of his class. His friends gave him the nickname "Toby," and people continued to call him that even when he was an old man.</p>
+        `
+    },
+    "1883": {
+        title: "The Youngest Speaker",
+        image: "../assets/img/barton_as_speaker_vintage.png",
+        content: `
+            <p>After becoming a lawyer, Barton entered politics. At just 33 years old, he was chosen to be the 'Speaker' of the Parliament. This was a very important job.</p>
+            <p>As Speaker, he had to keep order when politicians were shouting at each other. He was the youngest person ever to have this job in New South Wales.</p>
+        `
+    },
+    "1891": {
+        title: "The First Draft",
+        image: "../assets/img/barton_convention_speech_scene.png",
+        content: `
+            <p>Barton was chosen to help write the very first draft of the Australian Constitution. This was a giant document that explained how the new country would work.</p>
+            <p>He worked on a boat in the middle of Sydney Harbour with other leaders to get the words just right. He believed that Australia needed to be one nation to be strong.</p>
+        `
+    },
+    "1897": {
+        title: "Leader of the Convention",
+        image: "../assets/img/barton_convention_speech_scene.png",
+        content: `
+            <p>In 1897, the people of the colonies voted for leaders to finish writing the Constitution. Barton received more votes than anyone else in Australia!</p>
+            <p>He was chosen to be the leader of the whole meeting. He worked day and night, often through the night, to solve arguments between the different colonies and finish the rules.</p>
+        `
+    },
+    "1901": {
+        title: "The Big Day",
+        image: "../assets/img/federation_day_centennial_park_1901.png",
+        content: `
+            <p>On January 1, 1901, thousands of people gathered in Centennial Park, Sydney, to celebrate the birth of Australia.</p>
+            <p>Barton stood in a beautiful white pavilion and was sworn in as Australia's very first Prime Minister. It was the proudest moment of his life.</p>
+        `
+    },
+    "1903": {
+        title: "Moving to the High Court",
+        image: "../assets/img/edmund_barton_portrait_vintage.png",
+        content: `
+            <p>After serving as Prime Minister for nearly three years, Barton decided to become a judge on the new High Court of Australia.</p>
+            <p>He was an excellent judge and spent the rest of his life making sure that the laws in Australia were fair and followed the Constitution he had helped to write.</p>
+        `
+    }
+};
